06a98f35c774985b653d6ea5de2a08e226ede429
Sai Vardhan <vardhan.6405@gmail.com>
2024-01-18 23:20:45 +0530

    entry page and index page finished

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+import markdown2
 
 from . import util
 
@@ -8,3 +10,12 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def wiki(request, name):
+    entry = util.get_entry(name)
+    if entry != None:
+        html = markdown2.markdown(entry)
+        return render(request, f"encyclopedia/wikipage.html", {
+            "html": html,
+            "title": name.upper()
+        })
+    return render(request, "encyclopedia/notfound.html")
