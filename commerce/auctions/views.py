232f709679798310de3bee707ebb4ad26af43360
Sai Vardhan <vardhan.6405@gmail.com>
2024-01-27 12:09:24 +0530

    implemented Active listings page

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -9,7 +9,10 @@ from .models import *
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    listings = AuctionListing.objects.all()
+    return render(request, "auctions/index.html", {
+        "listings": listings
+    })
 
 
 def login_view(request):
